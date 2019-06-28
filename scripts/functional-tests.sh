3c38a27db4e835239a96d12a03f9abb3c72dcf51
Daniel Cook <danielecook@gmail.com>
2019-06-28 10:04:21 +0100

    add row numbers, finish select command

--- scripts/functional-tests.sh
+++ scripts/functional-tests.sh
@@ -3,34 +3,59 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 
 . ssshtest
 
+PARENT_DIR="`git rev-parse --show-toplevel`"
+export PATH="${PATH}:${PARENT_DIR}"
+
 set -o nounset
 
-assert_equal 18 12
-
-# slice
-run slice_inf csv slice 5: tests/data/*.tsv
+#=======#
+# slice #
+#=======#
+run slice_inf tut slice 5: tests/data/*.tsv
 assert_exit_code 0
 assert_no_stderr
 assert_equal "18" $(cat $STDOUT_FILE | wc -l)
 assert_in_stdout "Dodge Challenger"
 
-run slice_low csv slice :3 tests/data/*.tsv
+run slice_low tut slice :3 tests/data/*.tsv
 assert_exit_code 0
 assert_no_stderr
 assert_equal "12" $(cat $STDOUT_FILE | wc -l)
 assert_in_stdout "Fiat 128"
 
-run slice csv slice 1:3 tests/data/*.tsv
+run slice tut slice 1:3 tests/data/*.tsv
 assert_exit_code 0
 assert_no_stderr
 assert_equal "9" $(cat $STDOUT_FILE | wc -l)
 
-run slice csv slice huh:3 tests/data/*.tsv
+run slice tut slice huh:3 tests/data/*.tsv
 assert_exit_code 1
 assert_stderr
 assert_in_stderr "Malformed range"
 
-run slice_add_col csv slice -a 0:3 tests/data/*.tsv
+run slice_add_col tut slice -a 0:3 tests/data/*.tsv
 assert_exit_code 0
-assert_in_stdout "filename"
-assert_equal "12" $(cat $STDOUT_FILE | cut -f 1 | uniq | wc -l)
+assert_in_stdout "basename"
+assert_equal $(cat $STDOUT_FILE | cut -f 1 | uniq | wc -l) "12"
+assert_equal $(cat $STDOUT_FILE | cut -f 7 | head -n 1) "basename"
+assert_equal $(cat $STDOUT_FILE | cut -f 7 | head -n 2 | tail -n 1) "df1.tsv"
+
+#========#
+# select #
+#========#
+run select_1 tut select 1,2,3 tests/data/*.tsv
+assert_exit_code 0
+assert_no_stderr
+assert_in_stdout "mpg"
+assert_in_stdout "cyl"
+assert_in_stdout "disp"
+
+
+# select missing column
+run select_2 tut select mpg tests/data/df3.tsv
+assert_in_stdout "mpg" # header should be included
+assert_equal $(cat $STDOUT_FILE | uniq | wc -l) "2"
+
+# basename
+run select_3 tut select -b mpg tests/data/df1.tsv
+assert_equal $(cat $STDOUT_FILE | cut -f 2 | tail -n 1) "df1.tsv"
--- tut.nim
+++ tut.nim
@@ -12,8 +12,37 @@ import terminal
 from posix import signal, SIG_PIPE, SIG_IGN
 signal(SIG_PIPE, SIG_IGN)
 
+
+var PRINT_RN = false
+var ROW_NUMBER = 0
+
+proc print_row(line: seq[string], delim: string) =
+    if PRINT_RN:
+        if ROW_NUMBER == 0:
+            echo "rn"  & delim & line.join(delim)
+        else:
+            echo $ROW_NUMBER & delim & line.join(delim)
+        ROW_NUMBER += 1
+    else:
+        echo line.join(delim)
+
+proc print_row(line: string, delim: string) =
+    if PRINT_RN:
+        if ROW_NUMBER == 0:
+            echo "rn"  & delim & line
+        else:
+            echo $ROW_NUMBER & delim & line
+        ROW_NUMBER += 1
+    else:
+        echo line
+
+
 proc quit_error*(msg: string, error_code = 1) =
     stderr.write_line "Error".bgWhite.fgRed & fmt": {msg}".fgRed
+    quit(error_code)
+
+proc quit_error*(E: ref Exception, error_code = 1) =
+    stderr.write_line ($E.name).bgWhite.fgRed & fmt": {E.msg}".fgRed
     quit(error_code)
 
 
@@ -42,7 +71,39 @@ proc parse_header(path: string, delim = "<auto>"): (seq[string], string) =
             parsed_header.add(x.strip(chars = {'\"', '\''}))
     return (parsed_header, sep)
 
-proc stack(files: seq[string], sep: var string, output_sep: var string) =
+proc add_annotation_cols(line: var seq[string], n: int, add_col: bool, fname: string, add_basename: bool, add_filename: bool) =
+    # Adds annotation information to the line
+    # add_col - the mode to operate under; Is the column being added or updated?
+    if add_col:
+        if add_basename or add_filename:
+            if add_filename:
+                if n > 0:
+                    line.add fname
+                else:
+                    line.add("filename")
+            if add_basename:
+                if n > 0:
+                    line.add(os.lastPathPart(fname))
+                else:
+                    line.add("basename")
+    else:
+        if add_basename or add_filename:
+            if n == 0:
+                if add_basename:
+                    line.add("basename")
+                if add_filename:
+                    line.add("filename")
+            else:
+                if add_basename and add_basename:
+                    line[^1] = os.lastPathPart(fname)
+                    line[^2] = fname
+                elif add_basename:
+                    line[^1] = os.lastPathPart(fname)
+                elif add_filename:
+                    line[^1] = fname
+
+
+proc stack(files: seq[string], sep: var string, output_sep: var string, add_filename: bool, add_basename: bool) =
     var
         stack_header: seq[string]
         header: seq[string]
@@ -69,9 +130,9 @@ proc stack(files: seq[string], sep: var string, output_sep: var string) =
         # Print header
         (header, delim) = parse_header(path)
         var col_out = newSeq[string](stack_header.len)
-        var ln = 0
+        var n = 0
         for line in lines(path):
-            if ln > 0:
+            if n > 0:
                 var sep_use = sep
                 var cols: seq[string]
                 for x in line.split(sep):
@@ -81,17 +142,17 @@ proc stack(files: seq[string], sep: var string, output_sep: var string) =
                     ])
                     if match_col > -1 and ncol <= (cols.len - 1):
                         col_out[match_col] = cols[ncol]
-                echo col_out.join(output_sep)
-            elif ln == 0:
+                print_row(col_out, output_sep)
+            elif n == 0:
                 if sep == "<auto>":
                     sep = infer_delim(line)
                 if output_sep == "<auto>":
                     output_sep = infer_delim(line)
             if header_out == false:
                 # Infer line delimiter of first file and output header
-                stdout.write_line stack_header.join(output_sep)
+                print_row(stack_header, output_sep)
                 header_out = true
-            ln += 1
+            n += 1
 
 proc slice(line_range: string, files: seq[string], add_filename: bool, add_basename: bool) =
     var
@@ -126,32 +187,79 @@ proc slice(line_range: string, files: seq[string], add_filename: bool, add_basen
             if n >= r_start and n <= r_end:
                 var line_out = @[line]
                 if add_filename or add_basename:
-                    if add_filename:
-                        if n > 0:
-                            line_out.add(f)
-                        else:
-                            line_out.add("filename")
-                    if add_basename:
-                        if n > 0:
-                            line_out.add(os.lastPathPart(f))
-                        else:
-                            line_out.add("basename")
-                    echo line_out.join(delim)
+                    add_annotation_cols(line_out, n, true, f, add_filename, add_basename)
+                    print_row(line_out, delim)
                 else:
-                    echo line
+                    print_row(line, delim)
             n += 1
             
-proc select(cols_string: string, files: seq[string], add_col: bool) =
+proc select(cols_string: string, files: seq[string], add_col: bool, sep: string, add_basename: bool, add_filename: bool) =
     let cols = cols_string.split(",")
+    var delim: string
+    var columns: seq[string]
+    var line_out = newSeq[string](cols.len)
+    var added_header = false
 
     # Determine what type of selection is happening
     try:
+        # If all columns are integers, we can simply output cols by index
         var select_cols = cols.map(parseInt)
-        
 
-    except:
-        # Select by column name
-        echo "G"
+
+        for file_n in 0..<files.len:
+            var path = files[file_n]
+            (columns, delim) = parse_header(path, sep)
+            var n = 0
+            for line in lines(path):
+                # If its the first file, ok to print the column header
+                if file_n == 0 or n > 0:
+                    add_annotation_cols(line_out, n, false, path, add_filename, add_basename)
+                    for i in 0..<select_cols.len:
+                        line_out[i] = line.split(delim)[select_cols[i]]
+                    print_row(line_out, delim)
+                n += 1
+
+    except ValueError:
+        #[
+             If columns are referenced by name, we'll need
+             to rely on a hash table to select them.
+        ]#
+        var select_cols = cols
+        var column_indices = newSeq[int](select_cols.len)
+
+
+        for file_n in 0..<files.len:
+            var n = 0
+            var path = files[file_n]
+            (columns, delim) = parse_header(path, sep)
+
+            for col in 0..<select_cols.len:
+                var col_index = columns.find(select_cols[col])
+                if col_index > -1:
+                    column_indices[col] = col_index
+                else:
+                    try:
+                        column_indices[col] = select_cols[col].parseInt
+                    except ValueError:
+                        column_indices[col] = -1
+                        # Show user warning here?
+                        discard
+            
+            for line in lines(path):
+                if file_n == 0 or n > 0:
+                    add_annotation_cols(line_out, n, false, path, add_filename, add_basename)
+                    var current_line = line.split(delim)
+                    for col in 0..<column_indices.len:
+                        if column_indices[col] > -1:
+                            line_out[col] = current_line[column_indices[col]]
+                        else:
+                            if n > 0:
+                                line_out[col] = ""
+                            else:
+                                line_out[col] = select_cols[col]
+                    print_row(line_out, delim)
+                n += 1
+
 
 proc check_file(fname: string): bool =
     if not fileExists(fname):
@@ -175,11 +283,15 @@ proc parse_file_list(fnames: seq[string]): seq[string] =
 
 
 
-var p = newParser("csv"):
+var p = newParser("tut"):
+    flag("--row-numbers", help="Add Row Numbers")
+    flag("--debug", help="Debug")
     help("Table Utilities")
 
     command("select"):
         flag("-a", "--add-filename", help="Create a right-most column for the filename")
+        flag("-b", "--add-basename", help="Create a right-most column for the basename")
+        option("-d", "--delimiter", help="The field separater", default="<auto>")
         arg("cols", nargs= 1, help="A comma-delimted list of column numbers or names. Use '0' for everything.")
         arg("files", nargs= -1, help="Path")
         help("Select columns by name or index")
@@ -190,7 +302,7 @@ var p = newParser("csv"):
                 quit_error("No files specified")
                 quit()
             else:
-                select(opts.cols, parse_file_list(opts.files), opts.add_filename)
+                select(opts.cols, parse_file_list(opts.files), opts.add_filename, opts.delimiter, opts.add_filename, opts.add_basename)
             quit()
 
     command("slice"):
@@ -212,9 +324,11 @@ var p = newParser("csv"):
     command("stack"):
         arg("files", nargs= -1, help="List files to stack")
         option("-t", "--threads", help="Threads")
-        option("-d", "--delimiter", help="What separater is used", default="<auto>")
+        option("-d", "--delimiter", help="The field separater", default="<auto>")
         option("-p", "--output-delimiter", help="Separater to output; Defaults to that found in first file", default="\t")
-        flag("-s", "--slugify", help="Slugify field names")
+        #flag("-s", "--slugify", help="Slugify field names")
+        flag("-a", "--add-filename", help="Create a right-most column for the filename")
+        flag("-b", "--add-basename", help="Create a right-most column for the basename")
         flag("-g", "--group", help="Add a column for the source field")
         flag("--debug", help="Debug")
         help("Combine delimited files by column")
@@ -225,7 +339,7 @@ var p = newParser("csv"):
                 quit_error("No files specified")
                 quit()
             var file_set = parse_file_list(opts.files)
-            stack(file_set, opts.delimiter, opts.outputDelimiter)
+            stack(file_set, opts.delimiter, opts.outputDelimiter, opts.add_filename, opts.add_basename)
             quit()
 
 # Check if input is from pipe
@@ -241,6 +355,9 @@ if commandLineParams().len == 0:
     quit()
 else:
     try:
+        var opts = p.parse(input_params)
+        if opts.row_numbers:
+            PRINT_RN = true
         p.run(input_params)
     except UsageError as E:
         input_params.add("-h")
@@ -250,5 +367,5 @@ else:
             stderr.write_line "Error".bgWhite.fgRed & fmt": {E.msg}".fgRed
             raise
         else:
-            quit_error(E.msg)
+            quit_error(E)
 
