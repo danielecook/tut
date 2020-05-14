d45311a295104025fac72f62fd70d0b1bd6f978d
Daniel E. Cook <danielecook@gmail.com>
2020-05-13 21:08:42 -0400

    upload artifacts

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -15,18 +15,19 @@ cd "${PARENT_DIR}" || exit 1
 nimble install
 
 # Download test data
-mkdir -p "${PARENT_DIR}/.benchmarks" && cd "${PARENT_DIR}/.benchmarks"
-if ! test -s "${PARENT_DIR}/.benchmarks/worldcitiespop_mil.csv"; then
+mkdir -p "${PARENT_DIR}/.data" && cd "${PARENT_DIR}/.data"
+mkdir -p "${PARENT_DIR}/benchmarks" 
+if ! test -s "${PARENT_DIR}/.data/worldcitiespop_mil.csv"; then
     wget http://burntsushi.net/stuff/worldcitiespop_mil.csv
 fi;
 
 export PATH="${PARENT_DIR}:${PATH}"
 
-cd "${PARENT_DIR}/.benchmarks" || exit 1
+cd "${PARENT_DIR}/.data" || exit 1
 
 # Select a column
-hyperfine --export-csv "select.benchmarks.csv" \
-          --export-markdown "select.md" \
+hyperfine --export-csv "${PARENT_DIR}/benchmarks/select.csv" \
+          --export-markdown "${PARENT_DIR}/benchmarks/select.md" \
           --runs 10 \
-         "xsv select 2 ${PARENT_DIR}/.benchmarks/worldcitiespop_mil.csv > out.select.txt" \
-         "tut select 2 ${PARENT_DIR}/.benchmarks/worldcitiespop_mil.csv > out.select.txt"
+         "xsv select 2 ${PARENT_DIR}/.data/worldcitiespop_mil.csv > out.select.txt" \
+         "tut select 2 ${PARENT_DIR}/.data/worldcitiespop_mil.csv > out.select.txt"
