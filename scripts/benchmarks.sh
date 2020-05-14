919565e7bd27af3aa8254973e93279559755f5eb
Daniel E. Cook <danielecook@gmail.com>
2020-05-13 21:01:08 -0400

    fix benchmarks

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -10,22 +10,23 @@ function cleanup {
 trap cleanup EXIT
 
 PARENT_DIR="$(git rev-parse --show-toplevel)" 
+
 cd "${PARENT_DIR}" || exit 1
 nimble install
 
 # Download test data
-mkdir -p "${PARENT_DIR}/.data" && cd "${PARENT_DIR}/.data"
-if ! test -s "${PARENT_DIR}/.data/worldcitiespop_mil.csv"; then
+mkdir -p "${PARENT_DIR}/.benchmarks" && cd "${PARENT_DIR}/.benchmarks"
+if ! test -s "${PARENT_DIR}/.benchmarks/worldcitiespop_mil.csv"; then
     wget http://burntsushi.net/stuff/worldcitiespop_mil.csv
 fi;
 
-mkdir -p "${PARENT_DIR}/benchmarks" && cd "${PARENT_DIR}/benchmarks"
-
 export PATH="${PARENT_DIR}:${PATH}"
 
+cd "${PARENT_DIR}/.benchmarks" || exit 1
+
 # Select a column
-hyperfine --export-csv "benchmarks/select.benchmarks.csv" \
-          --export-markdown "benchmarks/select.md" \
+hyperfine --export-csv "select.benchmarks.csv" \
+          --export-markdown "select.md" \
           --runs 10 \
-         "xsv select 2 ${PARENT_DIR}/.data/worldcitiespop_mil.csv > out.select.txt" \
-         "tut select 2 ${PARENT_DIR}/.data/worldcitiespop_mil.csv > out.select.txt"
+         "xsv select 2 ${PARENT_DIR}/.benchmarks/worldcitiespop_mil.csv > out.select.txt" \
+         "tut select 2 ${PARENT_DIR}/.benchmarks/worldcitiespop_mil.csv > out.select.txt"
